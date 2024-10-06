9b940c684f4ec66dba77a56c7f9af11f76d48817
Ian McFarlane <i.mcfarlane2002@gmail.com>
2024-10-05 23:29:47 -0400

    remove level one

--- game/levels/level_one/vent/vent_cover.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@export var _degrees_rotated: float = 125
-@export var _animation_duration: float = 1
-
-func _rotate_down() -> void:
-	var tween = get_tree().create_tween()
-	tween.tween_property(self, "global_rotation_degrees", _degrees_rotated, _animation_duration)
-	tween.set_trans(Tween.TRANS_BOUNCE)
